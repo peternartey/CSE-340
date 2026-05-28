867305dafea002db012d0ee8c52a4a2e6dbc060b
peter-nartey <pnartey@byupathway.edu>
2026-05-28 01:31:25 +0000

    cse-340

--- src/database/index.js
+++ src/database/index.js
@@ -1,13 +1,12 @@
 require("dotenv").config();
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.PGUSER || "postgres",
-  host: process.env.PGHOST || "localhost",
-  database: process.env.PGDATABASE || "cse340",
-  password: process.env.PGPASSWORD || "",
-  port: Number(process.env.PGPORT || 5432),
-  ssl: process.env.PGSSLMODE === "require" ? { rejectUnauthorized: false } : false
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
