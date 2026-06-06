26852614de59123461996e6c11cb805029b2da2b
peter-nartey <pnartey@byupathway.edu>
2026-06-06 18:21:46 +0000

    cse-340

--- src/database/index.js
+++
@@ -1,9 +0,0 @@
-// src/database/index.js
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } // required for Render Postgres
-});
-
-module.exports = pool;
